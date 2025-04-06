d185eab402c85193909d4910ad1c836ec1b7d393
Dhaarun-Abhimanyu <dhaarunabhimanyu@gmail.com>
2025-04-06 21:45:43 +0530

    Added logging

--- src/main/java/dev/dhaarun_abhimanyu/todolist/TodolistApplication.java
+++ src/main/java/dev/dhaarun_abhimanyu/todolist/TodolistApplication.java
@@ -1,5 +1,6 @@
 package dev.dhaarun_abhimanyu.todolist;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +9,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.annotation.PostConstruct;
+
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 @SpringBootApplication
 @Controller
-public class TodolistApplication {
+public class TodolistApplication{
+  
+  @Value("${server.port}")
+  private int port;
 
 	@RequestMapping("/")
   public String home() {
@@ -23,6 +29,11 @@ public class TodolistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
+  }
+
+  @PostConstruct
+  public void logPort(){ 
+    System.out.println("\n\n\n!!!!! Server running in http://localhost:"+port);
   }
 
 }
